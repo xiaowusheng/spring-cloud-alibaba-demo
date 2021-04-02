2a544be6fa389e4acb0c60b7ab91a75083a182c3
xiaowusheng@nongfadai.com <xiaowusheng163@163.com>
2021-04-02 17:25:05 +0800

    服务提供方添加配置中心、添加api网关

--- dubbo/dubbo-provider/src/main/java/org/example/service/EchoServiceImpl.java
+++ dubbo/dubbo-provider/src/main/java/org/example/service/EchoServiceImpl.java
@@ -1,13 +1,19 @@
 package org.example.service;
 
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @DubboService(protocol = { "dubbo" })
+@RefreshScope
 class EchoServiceImpl implements EchoService {
+
+	@Value("${name:sp-alibaba}")
+	private String name;
 
 	@Override
 	public String echo(String message) {
-		return "[echo] Hello, " + message;
+		return name + "[echo] Hello, " + message;
 	}
 
 }
